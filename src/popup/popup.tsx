217ee6d187cfda2e05b1cb2cb8807314d10d688d
Diego Sabajo <diegofranco711@gmail.com>
2022-11-22 08:42:44 -0300

    data fetching now working

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import './popup.css'
+import { fetchOpenWeatherData } from '../utils/api'
 
-const test = <img src='icon.png'/>
+const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData("Paramaribo")
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div>
+      <img src='icon.png' />
+    </div>
+  )
+}
 
 // const rootElement = document.getElementById('root')
 const rootEL = document.createElement('div')
 document.body.appendChild(rootEL)
 const root = ReactDOM.createRoot(rootEL)
-root.render(test)
+root.render(<App />)
